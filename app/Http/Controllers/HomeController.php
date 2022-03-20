240a1ba129c96e31dd756c3df1a2d78df144b48f
DJan Daudov <djandaudovv@gmail.com>
2022-03-21 01:57:43 +0200

    feat: install and conf php_codesniffer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,17 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $viewData = [];
         $viewData["title"] = 'Home Page - Online Shop';
         return view('home.index')->with("viewData", $viewData);
     }
 
-    public function about(){
+    public function about()
+    {
         /*$data1 = "About-us - Online Shop";
         $data2 = "About us";
         $description = "This is an about page..";
@@ -27,6 +30,5 @@ class HomeController extends Controller
         $viewData["description"] = "This is an about page ...";
         $viewData["author"] = "Developed by: Your Name";
         return view('home.about')->with("viewData", $viewData);
-
     }
 }
