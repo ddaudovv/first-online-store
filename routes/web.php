976586a918a90a95a55884bc6158bb31c408e2f5
DJan Daudov <djandaudovv@gmail.com>
2022-03-21 01:48:23 +0200

    fix: navigate buttons and routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     $viewData = [];
     $viewData["title"] = "Home Page - Online Shop";
     return view('home.index')->with("viewData", $viewData);
-});
+});*/
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
