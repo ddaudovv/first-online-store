3fb7b1e3ad691731717d24b60e232b03938b9163
DJan Daudov <djandaudovv@gmail.com>
2022-03-21 02:27:47 +0200

    feat: add product and show page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
             <div class="navbar-nav ms-auto">
                 <a class="nav-link active" href="{{route('home.index')}}">Home</a>
                 <a class="nav-link active" href="{{route('home.about')}}">About</a>
+                <a class="nav-link active" href="{{route('product.index')}}">Products</a>
             </div>
         </div>
     </div>
