f9c5c7126088b9eeb8a88be2dc26183f7d5992e6
DJan Daudov <djandaudovv@gmail.com>
2022-03-27 01:15:42 +0200

    fix: adding functionality to edit and delete in admin panel

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,12 @@ Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController
             ->name("admin.product.index");
 
 Route::post('admin/product/store', 'App\Http\Controllers\Admin\AdminProductController@store')
-            ->name("admin.product.store");
+    ->name("admin.product.store");
+
+Route::delete('admin/product/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')
+    ->name("admin.product.delete");
+
+Route::get('/admin/products/{id}/edit', 'App\Http\Controllers\Admin\AdminProductController@edit')
+    ->name("admin.product.edit");
+Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')
+    ->name("admin.product.update");
