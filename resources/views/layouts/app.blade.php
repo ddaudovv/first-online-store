4a153c34cf482c9af34ccb682207dbb65d6ec317
DJan Daudov <djandaudovv@gmail.com>
2022-03-28 01:43:04 +0300

    feat: adding cart page and controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
                 <a class="nav-link active" href="{{route('home.index')}}">Home</a>
                 <a class="nav-link active" href="{{route('home.about')}}">About</a>
                 <a class="nav-link active" href="{{route('product.index')}}">Products</a>
+                <a class="nav-link active" href="{{route('cart.index')}}">Cart</a>
                 <div class="vr bg-white mx-2 d-none d-lg-block"></div>
                 @guest
                     <a class="nav-link active" href="{{ route('login') }}">Login</a>
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,9 @@ Route::middleware('admin')->group(function () {
         ->name("admin.product.update");
 });
 
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+Route::get('/cart/delete', 'App\Http\Controllers\CartController@delete')->name("cart.delete");
+Route::post('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
 
 Auth::routes();
 
